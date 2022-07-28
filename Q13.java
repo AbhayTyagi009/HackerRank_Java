510055f7aeaa7ca84c7ece5068530ab195d1b7f1
AbhayTyagi009 <abhaytyagi2513@gmail.com>
2022-07-28 23:26:28 +1000

    Third Commit

--- Q13.java
+++ Q13.java
@@ -1,18 +1,26 @@
-// Java Substring
-import java.io.*;
+// Java Currency Formatter
 import java.util.*;
 import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
-public class Q13 
-{
+public class Q13
+{    
     public static void main(String[] args) 
     {
-        Scanner in = new Scanner(System.in);
-        String S = in.next();
-        int start = in.nextInt();
-        int end = in.nextInt();
-        System.out.println(S.substring(start, end));
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        
+        // Write your code here.
+        Locale indiaLocale = new Locale("en", "IN");
+
+        NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat india = NumberFormat.getCurrencyInstance(indiaLocale);
+        NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+
+        System.out.println("US: " + us.format(payment));
+        System.out.println("India: " + india.format(payment));
+        System.out.println("China: " + china.format(payment));
+        System.out.println("France: " + france.format(payment));
     }
 }
--- Q14.java
+++ Q14.java
@@ -1,4 +1,4 @@
-// Java String Revese
+// Java Strings Introduction
 import java.io.*;
 import java.util.*;
 
@@ -8,15 +8,13 @@ public class Q14
     {    
         Scanner sc=new Scanner(System.in);
         String A=sc.next();
-        String rev = "";
-        for(int i = A.length()-1; i >= 0; i--)
-            rev += A.charAt(i);
-        if (Objects.equals(rev, A))
+        String B=sc.next();
+        System.out.println(A.length()+B.length());
+        if(A.charAt(0)>B.charAt(0))
             System.out.println("Yes");
-        else 
+        else
             System.out.println("No");
+        System.out.print(A.substring(0,1).toUpperCase()+A.substring(1)+" ");
+        System.out.print(B.substring(0,1).toUpperCase()+B.substring(1));
     }
 }
-
-
-
